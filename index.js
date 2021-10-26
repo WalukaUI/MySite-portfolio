90d196a566c0c84fa9bfae3761a19a75d8afd026
WalukaUI <cwaluka@yahoo.com>
2021-10-26 01:07:04 -0700

    fade in style added

--- index.js
+++ index.js
@@ -5,14 +5,38 @@ function iframeRef(frameRef) {
 let inside = iframeRef(document.getElementById("one"));
 
 function Listener() {
-  let links = document.getElementsByTagName("a");
+  let linksed = document.getElementById("urls");
+  links = linksed.getElementsByTagName("a");
   for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("click", (x) => {
-        document.getElementById("openBrowser").href=""
+      document.getElementById("openBrowser").href = "";
       document.getElementById("one").src = x.target.name;
       document.getElementById("openBrowser").href = x.target.name;
     });
   }
 }
+Listener();
 
-document.addEventListener("DOMContentLoaded", Listener());
+const sliders=document.querySelectorAll('.slide-in')
+const options={
+    threshold:0,
+    rootMargin:"-250px"
+}
+const apperOnScroll= new IntersectionObserver(function(entries){
+entries.forEach(entry =>{
+   
+
+    if(!entry.isIntersecting){
+        return;
+    }
+     console.log(entry.target);
+    entry.target.classList.add('appear')
+    apperOnScroll.unobserve(entry.target)
+})
+},options);
+sliders.forEach(fader=>{
+    apperOnScroll.observe(fader)
+})
+
+
+//document.addEventListener("DOMContentLoaded", Listener());
