f4671910b00f2b1ab4722e4abb1949151ae09943
WalukaUI <cwaluka@yahoo.com>
2021-10-25 19:45:45 -0700

    sample div completed

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+function iframeRef(frameRef) {
+  return frameRef.src ? frameRef.src : frameRef.src;
+}
 
+let inside = iframeRef(document.getElementById("one"));
 
+function Listener() {
+  let links = document.getElementsByTagName("a");
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", (x) => {
+        document.getElementById("openBrowser").href=""
+      document.getElementById("one").src = x.target.name;
+      document.getElementById("openBrowser").href = x.target.name;
+    });
+  }
+}
 
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-})
+document.addEventListener("DOMContentLoaded", Listener());
